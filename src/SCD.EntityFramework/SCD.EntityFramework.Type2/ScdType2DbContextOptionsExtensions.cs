14626d89731135bff0642115472fcba89be770c3
armalt <armalt@users.noreply.github.com>
2024-05-14 21:01:32 +0200

    work progress

---
+++ src/SCD.EntityFramework/SCD.EntityFramework.Type2/ScdType2DbContextOptionsExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace SCD.EntityFramework.Type2;
+
+public static class ScdType2DbContextOptionsExtensions
+{
+    public static DbContextOptionsBuilder UseScdType2(this DbContextOptionsBuilder optionsBuilder)
+    {
+        if (optionsBuilder is null)
+        {
+            throw new ArgumentNullException(nameof(optionsBuilder));
+        }
+
+        // Check, whether the scd extension has already been added and if not it will add it.
+        var extension = optionsBuilder.Options.FindExtension<ScdType2Extension>() ?? new ScdType2Extension();
+        ((IDbContextOptionsBuilderInfrastructure)optionsBuilder).AddOrUpdateExtension(extension);
+        
+        return optionsBuilder;
+    }
+}
---
+++ src/SCD.EntityFramework/SCD.EntityFramework.Type2/ScdType2Extension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SCD.EntityFramework.Type2;
+
+public class ScdType2Extension : IDbContextOptionsExtension
+{
+    public void ApplyServices(IServiceCollection services)
+    {
+        
+    }
+
+    public void Validate(IDbContextOptions options)
+    {
+        
+    }
+
+    public DbContextOptionsExtensionInfo Info { get; }
+}
