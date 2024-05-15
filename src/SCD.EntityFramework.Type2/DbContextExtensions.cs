82668ab5e88bc7481afc030b7bfd16049c43b94f
armalt <armalt@users.noreply.github.com>
2024-05-15 09:45:24 +0200

    restructure solution setup

---
+++ src/SCD.EntityFramework.Type2/DbContextExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SCD.EntityFramework.Type2;
+
+public static class DbContextExtensions
+{
+    public static void EnsureScdType2(this DbContext dbContext)
+    {
+        // logic
+    }
+}
